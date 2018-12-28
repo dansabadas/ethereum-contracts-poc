13a903d8ddf441b1ecaf93a6044397385e9ed962
Dan Sabadis <dsabadis@sdl.com>
2018-12-28 15:42:18 +0200

    freezing Ethereum research

--- NethereumSample/Program.cs
+++ NethereumSample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Nethereum.Web3;
 
 namespace NethereumSample
 {
@@ -14,19 +13,8 @@ namespace NethereumSample
 
         static async Task Main()
         {
-            await GetAccountBalance();
+            await AccountBalance.GetAccountBalance();
             Console.ReadLine();
-        }
-
-        private static async Task GetAccountBalance()
-        {
-            var web3 = new Web3("https://mainnet.infura.io");
-
-            var balance = await web3.Eth.GetBalance.SendRequestAsync("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae");
-            Console.WriteLine($"Balance in Wei: {balance.Value}");
-
-            var etherAmount = Web3.Convert.FromWei(balance.Value);
-            Console.WriteLine($"Balance in Ether: {etherAmount}");
         }
     }
 }
