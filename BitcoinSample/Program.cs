30f85f7d2725e08ca531589d73c69a8391046218
Dan Sabadis <dsabadis@sdl.com>
2018-12-28 18:08:20 +0200

    private key

--- BitcoinSample/Program.cs
+++ BitcoinSample/Program.cs
@@ -9,6 +9,22 @@ namespace BitcoinSample
         {
             var privateKey = new Key(); // generate a random private key
             BitcoinAddress(privateKey);
+
+            PrivateKey(privateKey);
+        }
+
+        private static void PrivateKey(Key privateKey)
+        {
+            // generate our Bitcoin secret(also known as Wallet Import Format or simply WIF) from our private key for the mainnet
+            BitcoinSecret mainNetPrivateKey = privateKey.GetBitcoinSecret(Network.Main);
+
+            // generate our Bitcoin secret(also known as Wallet Import Format or simply WIF) from our private key for the testnet
+            BitcoinSecret testNetPrivateKey = privateKey.GetBitcoinSecret(Network.TestNet); 
+            Console.WriteLine($"mainNetPrivateKey={mainNetPrivateKey}"); 
+            Console.WriteLine($"testNetPrivateKey={testNetPrivateKey}");
+
+            bool WifIsBitcoinSecret = mainNetPrivateKey == privateKey.GetWif(Network.Main);
+            Console.WriteLine($"WifIsBitcoinSecret={WifIsBitcoinSecret}");
         }
 
         private static void BitcoinAddress(Key privateKey)
@@ -31,6 +47,15 @@ namespace BitcoinSample
             mainNetAddress = publicKeyHash.GetAddress(Network.Main);
             Console.WriteLine($"mainNetAddress.ScriptPubKey={mainNetAddress.ScriptPubKey}");
             Console.WriteLine($"mainNetAddress.ScriptPubKey==testNetAddress.ScriptPubKey:{mainNetAddress.ScriptPubKey==testNetAddress.ScriptPubKey}");
+
+            var paymentScript = publicKeyHash.ScriptPubKey;
+            var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
+            Console.WriteLine($"mainNetAddress == sameMainNetAddress:{mainNetAddress == sameMainNetAddress}");
+
+            var samePublicKeyHash = (KeyId)paymentScript.GetDestination();
+            Console.WriteLine(publicKeyHash == samePublicKeyHash);
+            var sameMainNetAddress2 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
+            Console.WriteLine(mainNetAddress == sameMainNetAddress2);
         }
     }
 }
